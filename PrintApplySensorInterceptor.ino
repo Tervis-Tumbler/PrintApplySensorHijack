fea803d7ac2f17a50253cddef65f4b8e42da3324
Chris Magnuson <cmagnuson@tervis.com>
2016-06-08 00:08:52 -0400

    Output number of samples used to calculate velocity

--- PrintApplySensorInterceptor.ino
+++ PrintApplySensorInterceptor.ino
@@ -12,6 +12,8 @@ int yellowLED = 6;
 int spiCableSelect = 10;
 
 long accelerometerCurveArea = 0;
+int zSampleCount = 0;
+//int defaultZValue = 640;
 int accelerometerSampleDelay = 5;
 int applicatorDepartureSensor = 2;
 int cycleDelay = 2000;
@@ -19,8 +21,8 @@ int randomStrokeDelay;
 int registerAddress;
 long minVelocityToCalculate = 1000; // 9000
 long maxVelocityToCalculate = 55000; // 45000
-int maxRandomStrokeDelayTime = 100; // 120
-int minRandomStrokeDelayTime = 10; // 30
+int maxRandomStrokeDelayTime = 115; // 120
+int minRandomStrokeDelayTime = -10; // 30
 
 SPISettings settingsA(400000,MSBFIRST,SPI_MODE3);
 
@@ -98,12 +100,14 @@ int calculateRandomStrokeDelay(long input) { // Map Random Stroke Delay values t
   }
 }
 
-void printStatus(int randomStrokeDelay, long accelerometerCurveArea){
+void printStatus(int randomStrokeDelay, long accelerometerCurveArea, int zSampleCount){
   Serial.print("Random Stroke Delay: ");
   Serial.print(randomStrokeDelay);
   Serial.println(" ms");
   Serial.print("Velocity: ");
   Serial.println(accelerometerCurveArea);
+  Serial.print("Sample Count: ");
+  Serial.print(zSampleCount);
   Serial.println("");
 }
 
@@ -151,10 +155,13 @@ void printSettings() {
 }
 
 void getAverageVelocityUsingRiemannSums () {
+  zSampleCount = 0;
   while (!boxDetected()){
     delay(accelerometerSampleDelay);
+    zSampleCount += 1;
+    int ZValue = getZ()*-1;
     // Units are in 1/250th of a g. Add 250 units to offset gravity.
-    int netAccelerometerValue = (getZ()*-1) + 250;
+    int netAccelerometerValue = ZValue + 250;
     // Acceleration curve is estimated by grabbing samples of accelerometer values over time.
     // The average velocity is calculated by estimating the area of the acceleration curve, 
     // accelerometerCurveArea, using the Riemann sum method of calculating curve area.
@@ -183,7 +190,7 @@ void loop() {
     getAverageVelocityUsingRiemannSums();
     Serial.println("Box detected!");
     randomStrokeDelay = calculateRandomStrokeDelay(accelerometerCurveArea);
-    printStatus(randomStrokeDelay,accelerometerCurveArea);
+    printStatus(randomStrokeDelay, accelerometerCurveArea, zSampleCount);
     sendRandomStrokeDelaySignal();
     accelerometerCurveArea = 0;
     Serial.println("Waiting...");
